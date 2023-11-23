77d79fcb93398dd67995150f1055605831ceaffb
akrxso <tjgus991008@gmail.com>
2023-11-23 15:17:23 +0900

    서현

--- app/src/main/java/com/example/carrotmarket/SplashMainActivity.kt
+++ app/src/main/java/com/example/carrotmarket/SplashMainActivity.kt
@@ -4,16 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 
 @Suppress("DEPRECATION")
 class SplashMainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_main)
+
+        auth = Firebase.auth
         Handler().postDelayed(
             {
+                Firebase.auth.signOut()
+                Toast.makeText(this,"로그아웃 완료!", Toast.LENGTH_LONG).show()
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
+
             }, 2500)
     }
 }
